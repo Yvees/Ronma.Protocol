d9973410716f587152f6e89d5cef96f3b20178cf
YvesShen <KevinYeti@gmail.com>
2026-02-07 22:39:31 +0800

    update

--- src/Ronma.Protocol/Interface/Service/IService.cs
+++ src/Ronma.Protocol/Interface/Service/IService.cs
@@ -5,7 +5,7 @@ namespace Ronma.Protocol.Interface.Service
     public interface IService
     {
         ServiceInfo ServiceInfo { get; }
-        void Enroll();
-        void Unenroll();
+        Task Enroll();
+        Task Unenroll();
     }
 }
