671a61fa00ecc4488c67cc9b39ca00e86595d1b3
YvesShen <KevinYeti@gmail.com>
2026-02-09 21:42:49 +0800

    Create Agent.cs

---
+++ src/Ronma.Protocol/Enums/Agent.cs
@@ -0,0 +1,11 @@
+namespace Ronma.Protocol.Enums
+{
+    public enum AgentEventType
+    {
+        UserInput,
+        Intervention,
+        ServiceResult,
+        FollowUp,
+        System
+    }
+}
