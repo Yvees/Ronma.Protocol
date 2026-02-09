71afdc71cfdc427eb4ed551d592b4a4cc0064dbb
YvesShen <KevinYeti@gmail.com>
2026-02-10 07:31:23 +0800

    Update Packet.cs

--- src/Ronma.Protocol/Structure/Packet.cs
+++ src/Ronma.Protocol/Structure/Packet.cs
@@ -1,12 +1,14 @@
-﻿namespace Ronma.Protocol.Structure
+using System;
+
+namespace Ronma.Protocol.Structure
 {
     public class Packet
     {
         public Packet()
         {
-
         }
-        public Packet(string service, string command,  List<byte[]> args)
+
+        public Packet(string service, string command, List<byte[]> args)
         {
             Service = service;
             Command = command;
@@ -27,7 +29,17 @@
 
         public string Sender { get; set; }
 
+        public string TraceId { get; set; } = CreateTraceId();
+
         public List<byte[]> Args { get; set; }
 
+        private static string CreateTraceId()
+        {
+            var guid = Guid.CreateVersion7();
+            return Convert.ToBase64String(guid.ToByteArray())
+                .TrimEnd('=')
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
     }
 }
